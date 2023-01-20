a8f6ae52db9c7d13ac92e8108a6f0a880f4cf3d8
MoiseOcculis <97692598+MoiseOcculis@users.noreply.github.com>
2023-01-20 22:16:29 +0000

    Add files via upload
    Day 1 Assignments

---
+++ Assignment4.java
@@ -0,0 +1,10 @@
+package com.day1;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Object Oriented Programming:Day1 Session");
+		
+	}
+
+}
